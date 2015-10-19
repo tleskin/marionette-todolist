281c272cd729cbec1e64bb1062ba16a6506a25cf
Thomas Leskin <tleskin@gmail.com>
2015-10-18 20:20:20 -0600

    Refactor code, task can be added and deleted

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,47 +1,142 @@
 var TodoList = new Marionette.Application();
 
-TodoList.Item = Backbone.Model.extend({
-  defaults: {
-    title: '',
-    description: ''
-  }
+var Task = Backbone.Model.extend();
+
+var Tasks = Backbone.Collection.extend({
+  model: Task
 });
 
-TodoList.ListView = Marionette.ItemView.extend({
-  template: "#item-template",
+var TaskView = Backbone.Marionette.ItemView.extend({
+  template: '#taskView',
 
   events: {
-    "click p": 'alertItem'
+    'click button': 'completeTask'
   },
 
-  alertItem: function(){
-    alert(this.model.escape("description"));
+  completeTask: function(){
+    this.remove();
   }
 });
 
-TodoList.on("before:start", function(){
-  var RegionContainer = Marionette.LayoutView.extend({
-    el: "#app-container",
-
-    regions: {
-      main: "#main-region"
-    }
-  });
-
-  TodoList.regions = new RegionContainer();
+var NoTasksView = Backbone.Marionette.ItemView.extend({
+  template: '#noTasksView'
 });
 
-TodoList.on("start", function(){
-  var item = new TodoList.Item({
-    title: "Grocery shopping",
-    description: "Buy groceries for the week"
-  });
+var TasksView = Backbone.Marionette.CollectionView.extend({
+  emptyView: NoTasksView,
+  childView: TaskView
+});
 
-  var itemView = new TodoList.ListView({
-    model: item
-  });
+var FormView = Marionette.ItemView.extend({
+  template: '#formView',
 
-  TodoList.regions.main.show(itemView);
+  events: {
+    'click button': 'createNewTask'
+  },
+
+  ui: {
+    title: '#title',
+  },
+
+  createNewTask: function() {
+    this.collection.add({
+      title: this.ui.title.val(),
+    });
+
+    this.ui.title.val("");
+  }
+});
+
+TodoList.addRegions({
+  form: '#form',
+  list: '#list'
+});
+
+TodoList.addInitializer(function(){
+  TodoList.tasks = new Tasks();
+
+  TodoList.form.show(new FormView({ collection: TodoList.tasks}));
+  TodoList.list.show(new TasksView({ collection: TodoList.tasks}))
 });
 
 TodoList.start();
+
+
+
+// TodoList.ItemCollection = Backbone.Collection.extend({
+//   model: TodoList.Item,
+//
+//   comparator: 'title'
+// });
+//
+//
+
+// TodoList.ListItemView = Marionette.ItemView.extend({
+//   tagName: 'li',
+//   template: "#todo-list-item"
+// });
+//
+// var FormView = Marionette.ItemView({
+//   template: '#formView',
+//
+//   events: {
+//     'click button': 'createNewItem'
+//   },
+//
+//   ui {
+//     title: '#title',
+//     description: '#description'
+//   },
+//
+//   createNewItem: function() {
+//     this.collection.add({
+//       title: this.ui.title.val(),
+//       description: this.ui.description.val()
+//     });
+//
+//     this.ui.title.val("");
+//     this.ui.description.val("");
+//   }
+// });
+//
+// TodoList.ListItemsView = Marionette.CollectionView.extend({
+//   tagName: 'ul',
+//   childView: TodoList.ListItemView
+// });
+//
+// TodoList.on("before:start", function(){
+//   var RegionContainer = Marionette.LayoutView.extend({
+//     el: "#app-container",
+//
+//     regions: {
+//       main: "#main-region"
+//     }
+//   });
+//
+//   TodoList.regions = new RegionContainer();
+// });
+//
+// TodoList.on("start", function(){
+//   var items = new TodoList.ItemCollection([
+//     {
+//     title: "Grocery shopping",
+//     description: "Buy groceries for the week"
+//     },
+//     {
+//     title: "Gas",
+//     description: "Fill up the VW Jetta with gas"
+//     },
+//     {
+//     title: "Bills",
+//     description: "Pay rent and utilities"
+//     }
+//   ]);
+//
+//   var itemsListView = new TodoList.ListItemsView({
+//     collection: items
+//   });
+//
+//   TodoList.regions.main.show(itemsListView);
+// });
+//
+// TodoList.start();
